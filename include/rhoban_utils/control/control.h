5d18ad3b6a8c997a69575f698a0ade1d193de018
Gregwar <g.passault@gmail.com>
2018-06-10 22:14:03 +0200

    Adding k_i in control

--- include/rhoban_utils/control/control.h
+++ include/rhoban_utils/control/control.h
@@ -7,12 +7,17 @@ class Control
 {
     public:
         Control();
-        void update(float value);
+        void reset();
+        float update(float error, float dt = 1.0);
 
         // Minimum and maximum for the output
         float min, max;
         // Gain of the proportional 
         float k_p;
+        // Gain for the integration
+        float k_i;
+        // Accumulator
+        float acc;
         // Output of the controller
         float output;
 };
--- src/rhoban_utils/control/control.cpp
+++ src/rhoban_utils/control/control.cpp
@@ -6,20 +6,35 @@ namespace rhoban_utils
 Control::Control()
 {
     k_p = 1.0;
+    k_i = 0.0;
+    
+    acc = 0;
     min = -100.0;
-    max = -100.0;
+    max = 100.0;
 }
 
-void Control::update(float value)
+void Control::reset()
+{
+    acc = 0;    
+}
+
+float Control::update(float error, float dt)
 {
     // P gain
-    float tmp = k_p*value;
+    float tmp = k_p*error;
+    
+    // I gain
+    acc += k_i*error*dt;
+    if (acc > max) acc = max;
+    if (acc < min) acc = min;
+    tmp += acc;
 
     // Bounding
     if (tmp < min) tmp = min;
     if (tmp > max) tmp = max;
 
     output = tmp;
+    return output;
 }
 
 }
