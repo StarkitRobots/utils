a6fd6b6401b4e741dd11627ebd4bd2ad72669564
medrimonia <medrimonia@gmail.com>
2018-08-24 20:30:01 +0200

    Implementing StringTable, not compiling yet

---
+++ include/rhoban_utils/tables/string_table.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace rhoban_utils {
+
+/// A string table is a table in which each of the column is named
+class StringTable {
+public:
+  typedef std::vector<std::string> Column;
+  
+  StringTable();
+  StringTable(const std::string & column_names,
+              std::map<std::string, Column> data);
+
+  /// Presence of 'separator' in column content is not supported
+  static StringTable buildFromFile(const std::string & file_path,
+                                   char separator=',',
+                                   bool has_header = true);
+
+  static StringTable buildFromString(const std::string & str,
+                                     char separator=',',
+                                     bool has_header = true);
+
+  size_t nbCols() const;
+  size_t nbRows() const;
+
+  /// Throws std::out_of_range if column_name does not exist
+  const Column & getColumn(const std::string & column_name) const;
+
+  /// Return a map with column_name as key and content as value
+  std::map<std::string, std::string> getLine(int row) const;
+  
+private:  
+  std::vector<std::string> column_names;
+
+  /// Stored by column_name
+  std::map<std::string, Column> data;
+};
+
+}
---
+++ src/rhoban_utils/tables/string_table.cpp
@@ -0,0 +1,94 @@
+#include "rhoban_utils/tables/string_table.h"
+
+#include "rhoban_utils/io_tools.h"
+#include "rhoban_utils/util.h"
+
+namespace rhoban_utils {
+
+StringTable::StringTable() {}
+
+StringTable::StringTable(std::vector<std::string> column_names_,
+                         std::map<std::string,Column> data_)
+  : column_names(column_names_), data(data_)
+{
+}
+
+StringTable StringTable::buildFromFile(const std::string & file_path,
+                                       char separator=",",
+                                       bool has_header = true) {
+  return buildFromString(file2string(file_path), separator, has_header);
+}
+
+StringTable StringTable::buildFromString(const std::string & str,
+                                         char separator=",",
+                                         bool has_header = true) {
+  // Convert string to lines
+  std::vector<std::string> lines;
+  split(str, '\n', lines);
+  // If there are no lines throw error
+  if (lines.size() == 0) {
+    throw std::runtime_error(DEBUG_INFO + " no lines in string");
+  }
+  // Find out column names or generate them
+  std::vector<std::string> column_names;
+  int first_line_idx = 0;
+  if (has_header) {
+    first_line_idx = 1;
+    split(lines[0], separator, column_names);
+  } else {
+    // Number of columns is defined by first line columns
+    std::vector<std::string> tmp;
+    split(lines[0], separator, tmp);
+    for (size_t col = 0; col < tmp.size(); col++) {
+      column_names.push_back("col_" + std::to_string(col));
+    }
+  }
+  // Fill content
+  std::map<std::string, Column> data;
+  for (const std::string & col : column_names) {
+    data[col] = std::vector<std::string>();
+  }
+
+  for (int line_idx = first_line_idx; line_idx < lines.size(); line_idx+) {
+    const std::string & line = lines[line_idx];
+    std::vector<std::string> values;
+    split(line, separator, values);
+    if (values.size() != column_names.size()) {
+      throw std::runtime_error(DEBUG_INFO +
+                               " invalid row " + std::to_string(line_idx) +
+                               ": expecting " + std::to_string(column_names.size()) +
+                               " columns, " + std::to_string(values.size()) + " received");
+    }
+    for (int col = 0; col < values.size(); col++) {
+      data[column_names[col]].push_back(values[col]);
+    }
+  }
+  return StringTable(column_names, data);
+}
+
+size_t StringTable::nbCols() const {
+  return column_names.size();
+}
+
+size_t StringTable::nbRows() const {
+  return data[column_names[0]].size();
+}
+
+const Column & StringTable::getColumn(const std::string & column_name) const {
+  return data.at(column_name);
+}
+
+std::map<std::string, std::string> StringTable::getLine(int row) const {
+  if (row >= nbRows()) {
+    throw std::out_of_range(DEBUG_INFO + "asking for row " + std::to_string(row) +
+                            " while table has " + std::to_string(nbRows()) +
+                            " rows (indices start at 0)");
+  }
+  std::map<std::string, std::string> result;
+  for (const std::string col_name : column_names) {
+    result[col_name] = data[col_name][row];
+  }
+  return result;
+}
+
+}
